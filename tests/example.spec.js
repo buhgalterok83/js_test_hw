edc09034d657a7ba9bf3faea964989fb1cde1f55
honcharovv <bughalterok83@gmail.com>
2024-07-02 15:40:08 +0300

    js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,29 +2,28 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('SauceDemo Tests', () => {
 
-  test('Login with valid credentials', async ({ page }) => {
+  test.beforeEach(async({ page }) => {
     await page.goto('https://www.saucedemo.com/');
-
     await page.fill('#user-name', 'standard_user');
     await page.fill('#password', 'secret_sauce');
     await page.click('#login-button');
-
     await page.waitForSelector('.inventory_list');
+  })
+
+  test('Login with valid credentials', async ({ page }) => {
+
     const inventoryHeader = await page.locator('.title').innerText();
     expect(inventoryHeader).toBe('Products');
+
   });
 
   test('Add item to cart', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'secret_sauce');
-    await page.click('#login-button');
-    await page.waitForSelector('.inventory_list');
 
     await page.click('.inventory_item button');
 
     const cartBadge = await page.locator('.shopping_cart_badge').innerText();
     expect(cartBadge).toBe('1');
+
   });
 
 });
